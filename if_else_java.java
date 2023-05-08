0dbac796538f08321e4d6be0350771a0d8632167
shakthi87 <119774135+shakthi87@users.noreply.github.com>
2023-05-08 14:13:09 +0530

    Update and rename if_else_java to if_else_java.java

--- if_else_java.java
+++ if_else_java.java
@@ -41,21 +41,16 @@ public class Solution {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
             
         scanner.close();
-        if (N%2 != 0){
+        if (N%2==1)
             System.out.println("Weird");
-        }
-        if (N%2==0)
-        {
-            if(N<=2 && N>=5){
+           
+          if (N%2==0)
+          {
+            if(N>=2 && N<=5 || N>20)
                 System.out.println("Not Weird");
-            }
-            else if(N<=6 && N>=20){
+            else if(N>=6 && N<=20)
                 System.out.println("Weird");
-            }
-        }
-        if (N>=20){
-            System.out.println("Not Weird");
-        }
-          
+          }
+         
     }
 }
